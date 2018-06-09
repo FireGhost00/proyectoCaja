985466d48728c2f6dac808551ecd607c3614db88
fireghost <n00gomez00@gmail.com>
2018-06-08 20:24:53 -0600

    Finalice el proyecto

---
+++ src/caja/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caja;
+
+/**
+ *
+ * @author fireghst
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        Caja obj1 = new Caja(3,2,6);
+         
+        System.out.println("El resultado es: "+obj1.Operacion());
+    }
+}
